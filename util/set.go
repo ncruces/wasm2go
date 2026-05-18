c0c6fe20b70cf7f6e982880f936b8f3ecfe27129
Nuno Cruces <ncruces@users.noreply.github.com>
2026-05-18 07:12:31 +0100

    Fix dups (#24)

--- util/set.go
+++ util/set.go
@@ -2,8 +2,20 @@ package util
 
 type set[T comparable] map[T]struct{}
 
-func (s set[T]) add(t T) {
+func (s set[T]) add(t T) bool {
+	if _, ok := s[t]; ok {
+		return false
+	}
 	s[t] = struct{}{}
+	return true
+}
+
+func (s set[T]) del(t T) bool {
+	if _, ok := s[t]; ok {
+		delete(s, t)
+		return true
+	}
+	return false
 }
 
 func (s set[T]) has(t T) bool {
