c5f0b3ba91c0c8992df2f02064040ec6553d61d5
Nuno Cruces <ncruces@users.noreply.github.com>
2026-06-22 16:29:54 +0100

    Reorder

--- fncompiler.go
+++ fncompiler.go
@@ -622,12 +622,14 @@ func (fn *funcCompiler) cleanup() {
 		passes.RemoveBlankAssigns(fn.decl)
 		passes.RemoveUnusedLocals(fn.decl)
 		passes.InlineSwitchGotos(fn.decl)
-		passes.InlineSingleGotos(fn.decl)
-		passes.UnnestBlocks(fn.decl)
-		passes.UnnestCases(fn.decl)
-		passes.RemoveEmptyStmts(fn.decl)
-		passes.InlineGotoEnd(fn.decl)
-		passes.InlineGotoReturn(fn.decl)
+		for range 2 {
+			passes.RemoveEmptyStmts(fn.decl)
+			passes.InlineGotoEnd(fn.decl)
+			passes.InlineGotoReturn(fn.decl)
+			passes.InlineSingleGotos(fn.decl)
+			passes.UnnestBlocks(fn.decl)
+			passes.UnnestCases(fn.decl)
+		}
 		if passes.RemoveReceiver(fn.decl) {
 			fn.call.(*ast.ParenExpr).X = fn.decl.Name
 		}
